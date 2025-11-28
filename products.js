69a59636f07a15371eba3caba20736077d5affea
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-11-28 12:12:16 +0530

    Update products.js from Gist

--- products.js
+++ products.js
@@ -1,1 +1,28 @@
-const products = [];
+const products = [
+    {
+        "id": 4,
+        "name": "adsk",
+        "slug": "adsk",
+        "sku": "nsakdj",
+        "brand": "jksadjk",
+        "badge": "sad",
+        "availability": "oidoi",
+        "description": "jsadkj",
+        "details": "jsdlk",
+        "tags": "jas,adsadasdsa,ada",
+        "price": {
+            "old": "₹7747",
+            "new": "₹999"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/test-my-pypy/main/images/products/watch/4/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/test-my-pypy/main/images/products/watch/4/hover.webp"
+        },
+        "otherImages": [
+            "https://raw.githubusercontent.com/test-pages-xoxox/test-my-pypy/main/images/products/watch/4/other1.webp",
+            "https://raw.githubusercontent.com/test-pages-xoxox/test-my-pypy/main/images/products/watch/4/other2.webp"
+        ],
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20adsk.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20adsk%0ASKU%3A%20nsakdj%0APrice%3A%20%E2%82%B9999%0AAvailability%3A%20oidoi%0A%0APlease%20confirm%20my%20order."
+    }
+];
